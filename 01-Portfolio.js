7fa88798156ac6b26fbe5b0919de204164a97f31
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2021-08-15 15:53:58 -0700

    navbarLinks

--- 01-Portfolio.js
+++ 01-Portfolio.js
@@ -1,1 +1,41 @@
 //This will contain all JS
+const sections = [ 'divHome', 'divAboutMe', 'divPrevWork', 'divProjects', 'divContactMe']
+function pageLoad ()
+{
+    showSection(0);
+}
+function showSection (sectionIdx)
+{
+   // alert(sectionIdx);
+    sections.forEach((section, idx)=>{
+        if (sectionIdx===idx)
+        {
+            $("#"+section).show();
+        }
+        else {
+            $("#"+section).hide();
+        }
+    });
+}
+function contactMe(frm)
+{
+    
+
+    const data ={
+        yourName: frm.yourName.value,
+        yourEmail: frm.yourEmail.value
+    }
+    alert("Send this to server: " +JSON.stringify(data));
+
+    return false;
+/*
+    fetch ("http://localhost:5000/api/contact",
+    {
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        data: data
+    }).then ((resp)=>{
+
+    })
+    */
+}
